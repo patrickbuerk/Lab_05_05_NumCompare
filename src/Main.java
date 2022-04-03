55aaf26cd6a367bef547ad992ad4c778fd5763a7
patrickbuerk <98759563+patrickbuerk@users.noreply.github.com>
2022-04-03 16:19:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Input two separate numbers");
+        double firstNumber = 1;
+        double secondNumber =100;
+
+        if(firstNumber > secondNumber){
+            System.out.println("the first number "+firstNumber+ " is bigger than "+secondNumber);
+        }
+        else if (secondNumber > firstNumber){
+            System.out.println("the first number "+firstNumber + " is smaller than "+secondNumber);
+        }
+        else if (firstNumber == secondNumber){
+            System.out.println("the first number "+firstNumber + " is the same as "+secondNumber);
+
+
+        }
+        else System.out.println("You did not enter a number you entered "+firstNumber +" and " +secondNumber);
+        //when the first number is bigger than the second it will look like this
+        System.out.println("Input two separate numbers");
+        double firstNumber1 = 300;
+        double secondNumber1 =100;
+
+        if(firstNumber1 > secondNumber1){
+            System.out.println("the first number "+firstNumber1+ " is bigger than "+secondNumber1);
+        }
+        else if (secondNumber1 > firstNumber1){
+            System.out.println("the first number "+firstNumber1 + " is smaller than "+secondNumber1);
+        }
+        else if (firstNumber1 == secondNumber1){
+            System.out.println("the first number "+firstNumber1 + " is the same as "+secondNumber1);
+
+
+        }
+        else System.out.println("You did not enter a number you entered "+firstNumber1 +" and " +secondNumber1);
+        //if the numbers are the same it will look like this
+        System.out.println("Input two separate numbers");
+        double firstNumber2 = 100;
+        double secondNumber2 =100;
+
+        if(firstNumber2 > secondNumber2){
+            System.out.println("the first number "+firstNumber2+ " is bigger than "+secondNumber2);
+        }
+        else if (secondNumber2 > firstNumber2){
+            System.out.println("the first number "+firstNumber2 + " is smaller than "+secondNumber2);
+        }
+        else if (firstNumber2 == secondNumber2){
+            System.out.println("the first number "+firstNumber2 + " is the same as "+secondNumber2);
+
+
+        }
+        else System.out.println("You did not enter a number you entered "+firstNumber2 +" and " +secondNumber2);
+
+
+    }
+}
